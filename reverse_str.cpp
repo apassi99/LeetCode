5b6a09ef974ad0848969f1fa3dde348ef2a85055
apassi99 <apassi@live.com>
2015-11-18 22:05:50 -0500

    made a mistake using malloc

--- reverse_str.cpp
+++ reverse_str.cpp
@@ -54,7 +54,7 @@ void string_reverse1(char *str) {
 
 char *string_reverse2(const char * str) {
   unsigned int len = my_strlen(str);
-  char * cpy = (char *) malloc((len+1) * sizeof(char *));
+  char * cpy = (char *) malloc((len+1) * sizeof(char));
 
   int count = 0;
   for (int i = len - 1; i >= 0; i--) {
