0ee70d97300302406c5520e87d4b916db7eb9e12
apassi99 <apassi@live.com>
2018-02-27 20:14:38 -0500

    73. Set Matrix Zeroes https://leetcode.com/problems/set-matrix-zeroes/description/

---
+++ set_matrix_zeros.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        
+        if (matrix.empty() || matrix[0].empty()) {
+            return;
+        }
+        
+        bool isFirstRowZero = false;
+        bool isFirstColZero = false;
+        
+        for (int i = 0; i < matrix.size(); i++) {
+            if (matrix[i][0] == 0) {
+                isFirstRowZero = true;
+                break;
+            }
+        }
+        
+        for (int i = 0; i < matrix[0].size(); i++) {
+            if (matrix[0][i] == 0) {
+                isFirstColZero = true;
+                break;
+            }
+        }
+        
+        for (int i = 1; i < matrix.size(); i++) {
+            for (int j = 1; j < matrix[0].size(); j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+        
+        for (int i = 1; i < matrix.size(); i++) {
+            for (int j = 1; j < matrix[0].size(); j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        
+        if (isFirstRowZero) {
+            for (int i = 0; i < matrix.size(); i++) {
+                matrix[i][0] = 0;
+            }
+        }
+        
+        if (isFirstColZero) {
+            for (int i = 0; i < matrix[0].size(); i++) {
+                matrix[0][i] = 0;
+            }
+        }
+        
+    }
+};
