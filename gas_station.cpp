57b226a8fad8cef38a1ca763c1c12ac745241a1b
apassi99 <apassi@live.com>
2018-03-31 09:45:48 -0400

    134. Gas Station https://leetcode.com/problems/gas-station/description/

---
+++ gas_station.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        
+        int gasRemaining = 0;
+        int numGasStations = gas.size();
+        int i = 0, j = 0, nextGasStation = 0, startIndex = 0;
+        bool completeCircle = true;
+        
+        while(i < numGasStations) {
+            j = nextGasStation;
+            gasRemaining = 0;
+            completeCircle = true;
+            startIndex = nextGasStation;
+            do {
+                gasRemaining += gas[j];
+                nextGasStation++;
+                i++;
+                
+                if (gasRemaining < cost[j]) {
+                    completeCircle = false;
+                    break;
+                }
+                gasRemaining = gasRemaining - cost[j];
+                j = (j+1) % numGasStations;
+            } while (j != startIndex);
+
+            if (completeCircle) {
+                return startIndex;
+            }
+        }
+        
+        return -1;
+    }
+};
