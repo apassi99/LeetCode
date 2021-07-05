798a3561b6c0b8a8b34471f2b61376577b5dfc03
apassi99 <apassi@live.com>
2021-07-05 08:30:57 -0400

    simplify implementation

--- pascal_triangles.cpp
+++ pascal_triangles.cpp
@@ -1,48 +1,20 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> pascalTriangle;
-        vector<int> v;
-        v.push_back(1);
         
-        if (numRows >= 1) {
-            pascalTriangle.push_back(v);
+        vector<vector<int>> result;
+        
+        for (int row = 1; row <= numRows; row++)
+        {
+            vector<int> rowValues(row, 1);
+            result.push_back(rowValues);
+            
+            for (int i = 1; i < row-1; i++)
+            {
+                result[row-1][i] = result[row-2][i] + result[row-2][i-1];
+            }
         }
         
-        if (numRows >= 2) {
-            vector<int> v1;
-            v1.push_back(1);
-            v1.push_back(1);
-            pascalTriangle.push_back(v1);
-        }
-        
-        generateHelper(&pascalTriangle, numRows);
-        
-        return pascalTriangle;
-    }
-    
-private:
-    void generateHelper(vector<vector<int>> * pascalTriangle, int rowNum) {
-        if (rowNum <= 2) {
-            return;
-        }
-        
-        vector<int> lastRow = pascalTriangle->back();
-        vector<int> newRow;
-        
-        newRow.push_back(1);
-        for (int i = 0; i < lastRow.size() / 2; i++) {
-            newRow.push_back(lastRow[i] + lastRow[i+1]); 
-        }
-        
-        int size = (lastRow.size() % 2) ? newRow.size() : newRow.size() - 1;
-        
-        for (int i = size - 1; i >= 0; i--) {
-            newRow.push_back( newRow[i] );
-        }
-        
-        pascalTriangle->push_back(newRow);
-        generateHelper(pascalTriangle, rowNum - 1);
-        
+        return result;
     }
 };
